6342457246eea8aff3ce8327cb82752d283fa180
zhanglinxiao <zhanglinxiao@tianmtech.cn>
2023-02-14 13:10:19 +0800

    update .gitignore

--- config/view.php
+++ config/view.php
@@ -3,5 +3,5 @@ return [
     'paths' => [
         resource_path('views'),
     ],
-    'compiled' => realpath(storage_path('framework/views')),
+    'compiled' => realpath(storage_path('views')),
 ];
