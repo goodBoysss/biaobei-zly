a57a02dfd0f6c3987d5b5e6cf7cd802b757ddf52
zhanglinxiao <zhanglinxiao@tianmtech.cn>
2023-02-16 11:08:03 +0800

    modify

--- app/Views/Redirect/Cover.blade.php
+++ app/Views/Redirect/Cover.blade.php
@@ -1,7 +1,7 @@
 <html>
 <body style='margin: 0;position: relative;'>
 <img style='position: fixed ; top: 0;'
-     src='https://channel-prod.obs.cn-east-3.myhuaweicloud.com:443/open_by_browser.JPG?AccessKeyId=U6QEPWLSFURLXGYZKW9L&Expires=1707127797&Signature=s0Sbyykh6gA8GQ6ftx%2BfM0Bk2sE%3D'
+     src='https://tm-shorturl-prod.obs.cn-east-3.myhuaweicloud.com/cover/open/tip.jpg'
      width='100%'/>
 <a href="{{$info['origin_url']}}">
     <img style='margin-top:17.5vw' src='{{$info['cover_image_url']}}' width='100%'/>
