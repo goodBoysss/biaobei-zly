4441ea684c1b89bb51b69a1e4e60562d20668602
zhanglinxiao <zhanglinxiao@tianmtech.cn>
2023-02-21 15:00:23 +0800

    modify

--- app/Views/Redirect/Cover.blade.php
+++ app/Views/Redirect/Cover.blade.php
@@ -1,10 +1,24 @@
 <html>
+<style>
+    div,img, a, p {
+        padding: 0;
+        margin: 0;
+        width: 100%;
+    }
+</style>
+
 <body style='margin: 0;position: relative;'>
-<img style='position: fixed ; top: 0;'
-     src='https://tm-shorturl-prod.obs.cn-east-3.myhuaweicloud.com/cover/open/tip.jpg'
-     width='100%'/>
-<a href="{{$info['origin_url']}}">
-    <img style='margin-top:17.5vw' src='{{$info['cover_image_url']}}' width='100%'/>
-</a>
+
+<div style='position: fixed ; top: 0;z-index: 1000'>
+    <img style='' src='https://tm-shorturl-prod.obs.cn-east-3.myhuaweicloud.com/cover/open/tip.jpg'
+         width='100%'/>
+    <a href="{{$info['origin_url']}}" style="margin: 0;padding: 0;">
+        <p style="text-align: center;line-height: 2em;font-size: 2em;background-color: white;">
+            "浏览器"依旧出错？点击文字手动跳转</p>
+    </a>
+</div>
+
+
+<img style='padding-top:17.5vw;filter: grayscale(0%)' src='{{$info['cover_image_url']}}' width='100%'/>
 </body>
 <html>
