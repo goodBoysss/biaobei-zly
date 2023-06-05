3de46ae24eef41c99f588db69fccdc417d76b58d
zhanglinxiao <zhanglinxiao@tianmtech.cn>
2023-06-05 18:51:10 +0800

    modify

--- app/Views/Sound.blade.php
+++ app/Views/Sound.blade.php
@@ -23,6 +23,7 @@
     <h1>语音生成</h1>
     <select name="" id="voice_name" style="width: 100%;margin-bottom: 30px">
         <option value="v00022bbe39adcab88bbbdbb60d35b9e1028f8lznb">张凌云</option>
+        <option value="v000246e5f4505635f22d826ebeaf520f8f8aj5u9e">老婆_01</option>
     </select>
     <textarea name="" id="text" cols="30" rows="10" style="width: 100%" placeholder="输入文字"></textarea>
     <button style="margin-top: 30px" id="bt">生成语音</button>
